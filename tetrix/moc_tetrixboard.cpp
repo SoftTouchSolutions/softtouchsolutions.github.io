bacd2eda70e4a40d1b6fbedeb449f2cd4b536083
john.alamina <john.alamina@gmail.com>
2022-09-04 18:03:12 +0100

    60

--- tetrix/moc_tetrixboard.cpp
+++ tetrix/moc_tetrixboard.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../tetrixboard.h"
+#include "../tetrix/tetrixboard.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
