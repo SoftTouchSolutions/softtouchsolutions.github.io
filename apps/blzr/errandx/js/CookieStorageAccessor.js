2ee35880392937cea34bbba9e5e149652212029f
john.alamina <john.alamina@itoms.com>
2024-08-12 05:09:09 +0100

    login

--- apps/blzr/errandx/js/CookieStorageAccessor.js
+++ apps/blzr/errandx/js/CookieStorageAccessor.js
@@ -1,9 +1,23 @@
-﻿export function get()
-{
-    return document.cookie;
+﻿
+// Set a cookie
+export function set(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
 
-export function set(key, value)
-{
-    document.cookie = `${key}=${value}`;
+// Get a cookie
+export function get(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
