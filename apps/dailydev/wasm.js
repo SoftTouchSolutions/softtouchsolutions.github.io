403ff26b28674f56ebf8e5ab133efa6e679c3c4f
john alamina <john.alamina@pfintell.com>
2025-12-24 13:19:50 +0000

    js base path fixed

--- apps/dailydev/wasm.js
+++ apps/dailydev/wasm.js
@@ -1,3 +1,4 @@
-import init, { run } from './assets/ddapp-dxh3ba8825769987f73.js';
+﻿import init, { run } from './apps/dailydev/assets/ddapp-dxh3ba8825769987f73.js';
 export default init;
 export { run };
+
